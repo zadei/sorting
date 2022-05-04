03cee32fe55e9d4ab5c1d46327a3f7c1b165cc98
Zade Shahin <83564652+zadei@users.noreply.github.com>
2022-05-04 14:54:35 +0930

    Add files via upload

---
+++ QuickSort.cpp
@@ -0,0 +1,37 @@
+#include "QuickSort.h"
+#include <iostream>
+#include <vector>
+
+std::vector<int> QuickSort::sort(std::vector<int> list)
+{
+    std::vector<int> sortedList = list;
+
+    if (list.size() > 1)
+    {
+        int pivot = list[0];
+        std::vector<int> less;
+        std::vector<int> greater;
+
+        for (int i = 1; i < list.size(); i++)
+        {
+            if (list[i] <= pivot)
+            {
+                less.push_back(list[i]);
+            }
+            else
+            {
+                greater.push_back(list[i]);
+            }
+        }
+
+        std::vector<int> sortedLess = sort(less);
+        std::vector<int> sortedGreater = sort(greater);
+
+        sortedList.clear();
+        sortedList.insert(sortedList.end(), sortedLess.begin(), sortedLess.end());
+        sortedList.push_back(pivot);
+        sortedList.insert(sortedList.end(), sortedGreater.begin(), sortedGreater.end());
+    }
+
+    return sortedList;
+}
---
+++ RecursiveBinarySearch.h
@@ -0,0 +1,11 @@
+#ifndef RECURSIVEBINARYSEARCH_H
+#define RECURSIVEBINARYSEARCH_H
+#include <iostream>
+#include <vector>
+
+class RecursiveBinarySearch {
+    public:
+    bool search(std::vector<int> list, int target);
+};
+
+#endif
