abe4eec685e3c9e8ce96de5a7f7b8209e122916e
Zade Shahin <83564652+zadei@users.noreply.github.com>
2022-05-04 15:02:36 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,31 +5,15 @@
 #include <vector>
 
 int main() {
-    std::vector<int> list {1,7,4,2,8,9,3,6,5,4};
-
+    std::vector<int> list;
+    while(!std::cin.fail()) {
+        int input;
+        std::cin >> input;
+        list.push_back(input);
+    }
+    
     BubbleSort bs;
-    std::cout << "Unsorted list: ";
-    for (long unsigned int i = 0; i < list.size(); i++) {
-        std::cout << list[i] << " ";
-    }
-
-    std::vector<int> sortedList = bs.sort(list);
-
-    std::cout << "\nSorted list (BS): ";
-    for (long unsigned int i = 0; i < sortedList.size(); i++) {
-        std::cout << sortedList[i] << " ";
-    }
-
     QuickSort qs;
-    std::cout << "\nSorted list (QS): ";
-    std::vector<int> QuickSortedList = qs.sort(list);
-
-    for (int i = 0; i < QuickSortedList.size(); i++) {
-        std::cout << QuickSortedList[i] << " ";
-    }
-
-    // We will use the qs sorted list to test the recursive binary search
-    std::cout << "\n\nRecursive Binary Search" << std::endl;
     RecursiveBinarySearch rbs;
-    std::cout << "Number found at index: " << rbs.search(QuickSortedList, 8) << std::endl;
+
 }
